53865d086304b86d3cffd9b9ae08f0be7a513410
emersonCK <emerson.rufino.fs@gmail.com>
2022-05-06 22:09:01 -0300

    feature: ProjetoPHP
    aula 06/05/2022

---
+++ ProjetoPHP/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+//Aqui a gente recupera o que o usuário digitou e qual método HTTP ele utilizou
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
+
+//Instanciar classe Router
+$router = new \Aluno\ProjetoPHP\Router($method, $path);
+
+//ADICIONAR AS ROTAS VÁLIDAS ABAIXO
+$router->get('/ola-mundo', function(){
+    return "Ola mundo!";
+});
+
+$router->get('/exemplo', 'Aluno\ProjetoPHP\Controller\ExercicioController::exibir');
+$router->post('/exemplo', 'Aluno\ProjetoPHP\Controller\ExercicioController::exibirResultado');
+
+//ADICIONAR AS ROTAS VÁLIDAS ACIMA
+$result = $router->handler();
+
+if (!$result){
+    http_response_code(404);
+    echo "Página não encontrada";
+    die();
+}
+
+echo $result($router->getParams());
